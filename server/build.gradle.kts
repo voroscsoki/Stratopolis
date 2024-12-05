0a2633431e378af934352cde87dfed990da29ab8
voroscsoki <wcsongor02@gmail.com>
2024-12-05 10:31:55 +0100

    Changed back to JVM 21

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,6 +1,5 @@
 val mainName = "dev.voroscsoki.stratopolis.server.Main"
 plugins {
-    kotlin("jvm")
     application
     id("io.ktor.plugin") version "2.3.12"
     kotlin("plugin.serialization") version "2.0.20"
